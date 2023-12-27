86231df400ad39e44f62cccdfca1905a9bd9c4a8
ArmNadaryan <armennadaryan310@gmail.com>
2023-12-27 23:21:38 +0400

    Initial commit

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from src import data, data_preprocessing, training_the_model, model_accuracy
 def main():
     file_path = 'data/Data_CHF_Zhao_2020_ATE.csv'
 
-    # Load data
+    # Importing Data
     df = data.import_data(file_path)
 
-    # Explore and prepare data
+    # Get Data information and Split the columns
     data_preprocessing.data_information(df)
     X, y = data_preprocessing.ind_dep_columns(df)
 
     # Train the model
     y_test, y_pred = training_the_model.train_model(X, y)
 
-    # Evaluate the model
+    # Get model accuracies
     model_accuracy.accuracies(y_test, y_pred)
 
 
